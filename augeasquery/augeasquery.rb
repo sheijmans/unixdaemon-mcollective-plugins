3a4dbe2bea0a2d95fc905ba15798de8ed3770fc0
Stefan Heijmans <sheijmans@ziggo.nl>
2014-02-12 13:32:14 +0100

    Metadata only in DDL files

--- augeasquery/augeasquery.rb
+++ augeasquery/augeasquery.rb
@@ -2,14 +2,6 @@ module MCollective
   module Agent
     class Augeasquery<RPC::Agent
 
-      metadata :name => "Augeas Based Config Query",
-               :description => "Augeas Based Config Querying",
-               :author => "Dean Wilson",
-               :license => "GPLv2",
-               :version => "0.1",
-               :url => "http://www.unixdaemon.net/mcollective-plugins/",
-               :timeout => 20
-
       action 'query' do
 
         begin
